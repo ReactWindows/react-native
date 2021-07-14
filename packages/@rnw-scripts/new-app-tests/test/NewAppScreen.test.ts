a61ffa41f76bb9e17458123cc4364473f4f8e9a8
Nick Gerleman <ngerlem@microsoft.com>
2021-07-14 16:56:43 -0700

    testID selector

--- packages/@rnw-scripts/new-app-tests/test/NewAppScreen.test.ts
+++ packages/@rnw-scripts/new-app-tests/test/NewAppScreen.test.ts
@@ -6,6 +6,6 @@
  */
 
 test('Verify presence of NewAppScreen Header', async () => {
-  const header = await $('new-app-screen-header');
+  const header = await $('~new-app-screen-header');
   expect(await header.isDisplayed()).toBe(true);
 });
