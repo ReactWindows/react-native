104371dad514910f23e9b3fff401efcd1b3ce65d
Nick Gerleman <ngerlem@microsoft.com>
2021-07-14 17:30:52 -0700

    waitForExist

--- packages/@rnw-scripts/new-app-tests/test/NewAppScreen.test.ts
+++ packages/@rnw-scripts/new-app-tests/test/NewAppScreen.test.ts
@@ -7,5 +7,5 @@
 
 test('Verify presence of NewAppScreen Header', async () => {
   const header = await $('~new-app-screen-header');
-  expect(await header.isDisplayed()).toBe(true);
+  await header.waitForExist();
 });
